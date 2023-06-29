d3fe5bdf8c67b24877a452b54665afe7e230d3b6
Robin Bartsch <rxba.code@gmail.com>
2023-06-29 13:35:36 +0200

    configure setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,29 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='Hyperspectral Semantic Segmentation',
     version='0.1.0',
     author='Nick Theisen',
     author_email='nicktheisen@uni-koblenz.de',
-    packages=['hyperseg'],
+    packages=find_packages(),
     scripts=[],
     url='',
     license='LICENSE.txt',
     description='A framework for hyperspectral semantic segmentation experiments.',
     long_description=open('README.md').read(),
     install_requires=[
-        "torch"
+        "torch",
+        "torchvision",
+        "tensorboard",
+        "h5py",
+        "pytorch_lightning",
+        "tqdm",
+        "matplotlib",
+        "scikit-learn",
+        "scipy",
+        "imageio",
+        "tifffile",
+        "efficientnet_pytorch",
+        "torchsummary",
         ],
 )
