18205bb5b280dc5f77b52879bddd4333d97a0da3
Robin Bartsch <rxba.code@gmail.com>
2023-09-26 10:39:08 +0200

    Enable ExportSplitCB for HSIRoad.

--- hyperseg/datasets/callbacks.py
+++ hyperseg/datasets/callbacks.py
@@ -3,15 +3,22 @@ import numpy as np
 
 class ExportSplitCallback(Callback):
     def on_train_start(self, trainer, pl_module):
+        
+        if trainer.datamodule.__class__.__name__ == 'HSIRoad':
+            train_samples = np.array(trainer.datamodule.dataset_train.name_list)
+            val_samples = np.array(trainer.datamodule.dataset_val.name_list)
+            # is unfortunately the same as validation
+            test_samples = np.array(trainer.datamodule.dataset_val.name_list)
+        else:
 
-        # access full list of sample-names through
-        # trainer -> datamoudle -> train subset object -> original dataset object
-        sample_list = np.array(trainer.datamodule.dataset_train.dataset.samplelist())
+            # access full list of sample-names through
+            # trainer -> datamoudle -> train subset object -> original dataset object
+            sample_list = np.array(trainer.datamodule.dataset_train.dataset.samplelist())
 
-        # get list of indices of train, test and validation subsets
-        train_samples = np.array(sample_list[trainer.datamodule.dataset_train.indices])
-        val_samples = np.array(sample_list[trainer.datamodule.dataset_val.indices])
-        test_samples = np.array(sample_list[trainer.datamodule.dataset_test.indices])
+            # get list of indices of train, test and validation subsets
+            train_samples = np.array(sample_list[trainer.datamodule.dataset_train.indices])
+            val_samples = np.array(sample_list[trainer.datamodule.dataset_val.indices])
+            test_samples = np.array(sample_list[trainer.datamodule.dataset_test.indices])
         
         # store list of train, validation and test sample names in log-dir
         np.savetxt(trainer.logger.log_dir+'/train_samples.csv', train_samples, fmt='%s')
