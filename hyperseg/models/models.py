f89fb5d732c9cd97534b7d439408b22bc80d2e87
Nick <nicktheisen@gmx.de>
2023-10-27 11:01:15 +0200

    remove ss3fcn

--- hyperseg/models/models.py
+++ hyperseg/models/models.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from hyperseg.models.imagebased import UNet, AGUNet, SS3FCN
+from hyperseg.models.imagebased import UNet, AGUNet
 
 def replace_if_exists(paramname, defaultval, argdict):
     return argdict[paramname] if (paramname in argdict) else defaultval
